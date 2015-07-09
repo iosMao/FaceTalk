3a66cbbab669270a9ed3fdb8b9bab0af87a28545
Mao-MacPro <1002237954@qq.com>
2015-07-09 17:59:49 +0800

    add homeviewcontrol

---
+++ FaceTalk/facetalk/FTDHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTDHomeViewController.h
+//  FaceTalk
+//
+//  Created by Mao-MacPro on 15/7/9.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTDHomeViewController : UIViewController
+
+@end
