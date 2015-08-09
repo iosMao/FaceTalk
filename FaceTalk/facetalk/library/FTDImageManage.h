3e5247ac6390d4f476bc2f865f45c39f64e839cb
Mao-MacPro <1002237954@qq.com>
2015-08-09 12:17:05 +0800

    新增本地照片管理类

---
+++ FaceTalk/facetalk/library/FTDImageManage.h
@@ -0,0 +1,13 @@
+//
+//  FTDImageManage.h
+//  FaceTalk
+//
+//  Created by Mao-MacPro on 15/8/7.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTDImageManage : NSObject
+
+@end
---
+++ FaceTalk/facetalk/library/FTDImageManage.m
@@ -0,0 +1,13 @@
+//
+//  FTDImageManage.m
+//  FaceTalk
+//
+//  Created by Mao-MacPro on 15/8/7.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import "FTDImageManage.h"
+
+@implementation FTDImageManage
+
+@end
