6696b0d1ed03cb6cd744834934fc6b5aebc76d60
Mao-MacPro <1002237954@qq.com>
2015-08-14 15:15:34 +0800

    新增下载管理类

---
+++ FaceTalk/facetalk/home/creatperson/models/FTDDataPacketManager.h
@@ -0,0 +1,13 @@
+//
+//  FTDDataPacketManager.h
+//  FaceTalk
+//
+//  Created by Mao-MacPro on 15/8/14.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTDDataPacketManager : NSObject
+
+@end
---
+++ FaceTalk/facetalk/home/creatperson/models/FTDDataPacketManager.m
@@ -0,0 +1,13 @@
+//
+//  FTDDataPacketManager.m
+//  FaceTalk
+//
+//  Created by Mao-MacPro on 15/8/14.
+//  Copyright (c) 2015年 wen. All rights reserved.
+//
+
+#import "FTDDataPacketManager.h"
+
+@implementation FTDDataPacketManager
+
+@end
